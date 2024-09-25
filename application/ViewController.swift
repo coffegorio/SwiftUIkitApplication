1853a73000365a3388683bddb8d73ff25a374642
coffegorio <coffegorio@vk.com>
2024-09-25 03:09:51 +0300

    Initial Commit

---
+++ application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  application
+//
+//  Created by Егорио on 25.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
